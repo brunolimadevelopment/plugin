3a4ab6c4d598649ddd940ba58ff1caf6912b8678
Bruno Lima <brunolimadevelopment@gmail.com>
2023-03-27 22:10:40 -0300

    plugin

---
+++ plugin/src/exemplo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Resume texto
+ * 
+ * @param int $number1 primeiro valor
+ * @param int $number2 segundo valor
+ * @return int resultado da soma 
+ */
+
+function Soma(int $number1, int $number2): int 
+{
+   $resut = $number1 + $number2;
+
+   return $resut;
+}
---
+++ plugin/src/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    include_once 'exemplo.php';
+
+    $n1 = 10;
+    $n2 = 10;
+
+    echo Soma($n1, $n2);
